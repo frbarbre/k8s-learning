5a80425f3f4b82fa57aa61616110f16df3e13fc1
frbarbre <fr.barbre@gmail.com>
2025-08-12 14:54:13 +0200

    setup multi service deployment for application with frontend & backend

--- hello-world/hello-world-app/app/page.tsx
+++ hello-world/hello-world-app/app/page.tsx
@@ -1,14 +1,34 @@
+export const dynamic = "force-dynamic";
 
-export const dynamic = "force-dynamic"
+async function getData() {
+  try {
+    const res = await fetch(`${process.env.API_URL}/api/hello`);
+    if (!res.ok) {
+      console.log(res);
+      return null;
+    }
 
-export default function Home() {
+    const result = res.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <div className="flex flex-col min-h-svh items-center justify-center">
-     <p>Server</p>
-     <h2>{process.env.MESSAGE}</h2>
+      <p>Server</p>
+      <h2>{process.env.MESSAGE}</h2>
 
-     <p>Client</p>
-     <h2>{process.env.NEXT_PUBLIC_MESSAGE}</h2>
+      <p>Client</p>
+      <h2>{process.env.NEXT_PUBLIC_MESSAGE}</h2>
+
+      <p>Data from backend</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
